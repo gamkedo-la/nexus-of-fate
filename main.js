ab1e2e7e04015399e97f1a2683da64cdcc1f10af
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-04-19 17:11:17 +0500

    added Sky

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ window.onload = function() {
   var robot = new Robot('robot.png', canvas.width - 150, 242);
 
   function draw() {
+
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "darkblue"; 
+    context.fillRect(0, 0, canvas.width, canvas.height * 0.5); 
+
+    context.beginPath();
+    context.arc(canvas.width * 0.8, canvas.height * 0.2, 50, 0, 2 * Math.PI); 
+    context.fillStyle = "yellow"; 
+    context.fill();
+    context.closePath();
 
     player.draw(context);
     robot.draw(context);
