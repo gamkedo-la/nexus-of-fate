80eab32c346a6e1e3ee4b998680f7b212359b429
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-05-17 04:29:32 +0500

    Image sequence folder(Not for itch)

--- main.js
+++ main.js
@@ -3,18 +3,23 @@ window.onload = function() {
   var context = canvas.getContext('2d');
 
   var player = new Player('Player.png', 50, 242, 10, 1500, 100, 5, 2);
-  var robot = new Robot('robot.png', canvas.width - 150, 242);
+  var robot = new Robot('Robot.png', canvas.width - 150, 242);
 
   function draw() {
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    context.fillStyle = "#deb887"; 
+    context.fillRect(0, canvas.height * 0.9, canvas.width, canvas.height * 0.6);
+
     context.fillStyle = "darkblue"; 
     context.fillRect(0, 0, canvas.width, canvas.height * 0.5); 
 
+    
+    
     context.beginPath();
     context.arc(canvas.width * 0.8, canvas.height * 0.2, 50, 0, 2 * Math.PI); 
-    context.fillStyle = "yellow"; 
+    context.fillStyle = "white"; 
     context.fill();
     context.closePath();
 
