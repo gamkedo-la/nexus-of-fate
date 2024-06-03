f724c9203dbb7cb492c3c35009725d099256ac51
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-06-04 04:53:06 +0500

    Some changes in walk anim

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
   canvas = document.getElementById('myCanvas');
   context = canvas.getContext('2d');
-  player = new Fighter(input_keyboard, 'images/player_walk.png', 52, FLOOR_Y, 10);
+  player = new Fighter(input_keyboard, 'images/player_walk.png', 52, FLOOR_Y, 12);
   robot = new Fighter(input_ai, 'images/robot_walk.png', 600, FLOOR_Y, 10);
 
   function draw() {
