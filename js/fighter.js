37b6eff76f1f13ba90d266049b5e233b3a3d77b4
Christer Kaitila <breakdance@mcfunkypants.com>
2024-06-04 08:55:18 -0700

    fighter animations now use deltatime

--- js/fighter.js
+++ js/fighter.js
@@ -1,29 +1,47 @@
+const ANIM_FPS = 20; // how fast the animations play
+const ANIM_FRAMECOUNT = 12; // how many frames in the spritesheet
+const MOVE_SPEED = 10; // how fast the fighters move left and right
+const JUMP_POWER = -10; // how much upward velocity jump gives you
+const GRAVITY = 0.2; // how fast you accelerate while falling
+const FLOOR_Y = 240; // lowest possible Y coordinate
+
 class Fighter {
 
-  constructor(whichInput, imageSrc, initialX, initialY, speed) {
+  constructor(whichInput, imageSrc, initialX, initialY) {
     this.keys = {};
     this.getInput = whichInput;
     this.image = new Image();
     this.image.src = imageSrc;
     this.x = initialX;
     this.y = initialY;
-    this.speed = speed;
+    this.speed = MOVE_SPEED;
     this.speedY = 0;
     this.frameNum = 0;
-    this.frameWait = 3;
-    this.frameCount = 12; 
+    this.frameCount = ANIM_FRAMECOUNT; 
+    this.animationFPS = ANIM_FPS;
+    this.timeTillNextFrame = 0;
+    this.previousFrameTimestamp = 0;
   }
 
   draw() {
+
+    // measure the elapsed time since last frame
+    let now = performance.now() / 1000;
+    let deltaTime = now - this.previousFrameTimestamp;
+    this.previousFrameTimestamp = now;
+    
+    // change to the next frame if it is time
+    this.timeTillNextFrame -= deltaTime;
+    //console.log("frameNum="+this.frameNum+" deltaTime="+deltaTime.toFixed(2)+" timeTillNextFrame="+this.timeTillNextFrame.toFixed(2));
+    if (this.timeTillNextFrame <= 0) {
+      this.frameNum++;
+      if (this.frameNum >= this.frameCount) this.frameNum = 0;
+      this.timeTillNextFrame = 1 / this.animationFPS;
+    }
+    
     var frameW = this.image.width;
     var frameH = 1913 * 0.2;
-    if (this.frameWait-- < 0) {
-      this.frameNum++;
-      if (this.frameNum >= this.frameCount) {
-        this.frameNum = 0;
-      }
-      this.frameWait = 3;
-    }
+
     context.drawImage(this.image, 0, frameH * this.frameNum, frameW, frameH,
       this.x, this.y, frameW, frameH);
   }
@@ -70,7 +88,7 @@ class Fighter {
 
   jump() {
     if (this.y >= FLOOR_Y) {
-      this.speedY = -10;
+      this.speedY = JUMP_POWER;
     }
   }
   
--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 var canvas, context, player, robot;
-const GRAVITY = 0.2;
-const FLOOR_Y = 240;
 
 window.onload = function() {
 
   canvas = document.getElementById('myCanvas');
   context = canvas.getContext('2d');
   
-  player = new Fighter(input_keyboard, 'images/player_walk.png', 52, FLOOR_Y, 10);
-  robot = new Fighter(input_ai, 'images/robot_walk.png', 600, FLOOR_Y, 10);
+  player = new Fighter(input_keyboard, 'images/player_walk.png', 52, FLOOR_Y, MOVE_SPEED);
+  robot = new Fighter(input_ai, 'images/robot_walk.png', 600, FLOOR_Y, MOVE_SPEED);
 
   function draw() {
     
