194fb179af68d2db69974f489b4fa207cc70fc68
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-06-25 04:29:09 +0500

    Added new sky, Removed mountains

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-var bgImage1,bgImage2,bgImage3,bgImage4,bgImage5,bgImage6;
+var bgImage1,bgImage5,bgImage6;
 
 var background = {
     draw : function() {
@@ -20,21 +20,7 @@ var background = {
             bgImage1.src="images/background1.png";
             bgImage1.onload = function() { this.loaded=true; }
         }
-        if (!bgImage2) { 
-            bgImage2 = new Image();
-            bgImage2.src="images/background2.png";
-            bgImage2.onload = function() { this.loaded=true; }
-        }
-        if (!bgImage3) { 
-            bgImage3 = new Image();
-            bgImage3.src="images/background3.png";
-            bgImage3.onload = function() { this.loaded=true; }
-        }
-        if (!bgImage4) { 
-            bgImage4 = new Image();
-            bgImage4.src="images/background4.png";
-            bgImage4.onload = function() { this.loaded=true; }
-        }
+       
         if (!bgImage5) { 
             bgImage5 = new Image();
             bgImage5.src="images/background5.png";
@@ -47,9 +33,7 @@ var background = {
         }
 
         if (bgImage1 && bgImage1.loaded) context.drawImage(bgImage1,-player.x/7,0);
-        if (bgImage2 && bgImage2.loaded) context.drawImage(bgImage2,-player.x/6,0);
-        if (bgImage3 && bgImage3.loaded) context.drawImage(bgImage3,-player.x/5,0);
-        if (bgImage4 && bgImage4.loaded) context.drawImage(bgImage4,-player.x/4,0);
+      
         if (bgImage5 && bgImage5.loaded) context.drawImage(bgImage5,-player.x/3,0);
         // FOREGROUND: should be drawn AFTER the player sprites if it overlaps
         if (bgImage6 && bgImage6.loaded) context.drawImage(bgImage6,-player.x/2.5,0);
