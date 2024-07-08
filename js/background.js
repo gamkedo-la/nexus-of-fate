8394f564d4a1bd6828e33fea20e375ecb3b6b093
Christer Kaitila <breakdance@mcfunkypants.com>
2024-07-08 13:01:14 -0700

    clouds

--- js/background.js
+++ js/background.js
@@ -1,26 +1,13 @@
-var bgImage1,bgImage5,bgImage6;
+var bgImage1,bgImage5,bgImage6,cloudImage;
 
 var background = {
     draw : function() {
-        /*
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = "#deb887"; 
-        context.fillRect(0, canvas.height * 0.85, canvas.width, canvas.height * 0.7);
-        context.fillStyle = "darkblue"; 
-        context.fillRect(0, 0, canvas.width, canvas.height * 0.3); 
-        context.beginPath();
-        context.arc(canvas.width * 0.8, canvas.height * 0.2, 50, 0, 2 * Math.PI); 
-        context.fillStyle = "white"; 
-        context.fill();
-        context.closePath();
-        */
-        
+
         if (!bgImage1) { 
             bgImage1 = new Image();
             bgImage1.src="images/background1.png";
             bgImage1.onload = function() { this.loaded=true; }
         }
-       
         if (!bgImage5) { 
             bgImage5 = new Image();
             bgImage5.src="images/background5.png";
@@ -31,12 +18,18 @@ var background = {
             bgImage6.src="images/background6.png";
             bgImage6.onload = function() { this.loaded=true; }
         }
+        if (!cloudImage) { 
+            cloudImage = new Image();
+            cloudImage.src="images/clouds.png";
+            cloudImage.onload = function() { this.loaded=true; }
+        }
 
-        if (bgImage1 && bgImage1.loaded) context.drawImage(bgImage1,-player.x/7,0);
-      
-        if (bgImage5 && bgImage5.loaded) context.drawImage(bgImage5,-player.x/3,0);
-        // FOREGROUND: should be drawn AFTER the player sprites if it overlaps
-        if (bgImage6 && bgImage6.loaded) context.drawImage(bgImage6,-player.x/2.5,0);
+        if (bgImage1.loaded) context.drawImage(bgImage1,-player.x/7,0);
+        context.globalAlpha = 0.1;
+        if (cloudImage.loaded) context.drawImage(cloudImage,Math.sin(performance.now()/32000)*1000-1000-player.x/7,0);
+        context.globalAlpha = 1;
+        if (bgImage5.loaded) context.drawImage(bgImage5,-player.x/3,0);
+        if (bgImage6.loaded) context.drawImage(bgImage6,-player.x/2.5,0);
     }
 }
 
