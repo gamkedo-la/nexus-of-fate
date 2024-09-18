00ec55cb599ba8f5a185c39c644c501f13a68cbe
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-09-19 04:37:14 +0500

    Added intro and background music back

--- js/Music.js
+++ js/Music.js
@@ -1,16 +1,16 @@
 var music_is_playing = false;
 var music = new Audio('audio/dark_theme_by_mcfunkypants.mp3');
-music.volume = 0.0;
+music.volume = 0.5;
 music.loop = true;
 
 var INTRO_music_is_playing = false;
 var INTRO_music = new Audio('audio/intro_Music_Sample_By_Cindy_Andrade.mp3');
-INTRO_music.volume = 0.0;
+INTRO_music.volume = 0.5;
 INTRO_music.loop = true;
 console.log("autostarting intro music...");
 try {
-    //INTRO_music.play();
-   // INTRO_music_is_playing = true;
+     INTRO_music.play();
+     INTRO_music_is_playing = true;
 } catch(e) {
     console.log("intro music unable to autostart locally (but on itch.io this should work ok)");
     INTRO_music_is_playing = false;
