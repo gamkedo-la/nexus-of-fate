58e98b67fb67cdf3158ec180e54cdbb86e08e687
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-10-04 20:46:20 +0500

    Player enemy options buttons implemented

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -11,12 +11,29 @@ class MainMenu {
       var rect = canvas.getBoundingClientRect();
 
       return (mouseClick.clientX >= rect.left + 250 && mouseClick.clientX <= rect.left + 900 &&
-        mouseClick.clientY >= rect.top + 400 && mouseClick.clientY <= rect.top + 500);
+        mouseClick.clientY >= rect.top + 428 && mouseClick.clientY <= rect.top + 467);
+    }
+	
+	function userIsOnStartText2Player(mouseClick) {
+      // current Start text: clientX(610-900); clientY(500-600)
+      var rect = canvas.getBoundingClientRect();
+
+      return (mouseClick.clientX >= rect.left + 250 && mouseClick.clientX <= rect.left + 900 &&
+        mouseClick.clientY >= rect.top + 535 && mouseClick.clientY <= rect.top + 561);
     }
 
     window.addEventListener("click", (event) => {
 
       if (userIsOnStartText(event)) {
+		robot.AI = true;
+        this.onMainMenu = false;
+		console.log("Gameplay music");
+		INTRO_music.pause();
+		INTRO_music.currentTime = 0;
+		music.play();
+        fx.clear(); // remove smoke particles
+      } else if (userIsOnStartText2Player(event)) {
+		robot.AI = false;
         this.onMainMenu = false;
 		console.log("Gameplay music");
 		INTRO_music.pause();
@@ -24,12 +41,14 @@ class MainMenu {
 		music.play();
         fx.clear(); // remove smoke particles
       } else {
+		 
         // clicked empty space - trigger intro music if it was unable to autostart
         if (this.onMainMenu) {
             console.log("clicked empty space on title screen: start intro music!");
             INTRO_music.play(); INTRO_music_is_playing = true;
         }
       }
+	  
     });
 
     // Cursor Styling when over Start Text
