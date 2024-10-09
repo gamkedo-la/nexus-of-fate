f441c10a4e271c1f236773f8279bf39795bbfc81
Christer Kaitila <breakdance@mcfunkypants.com>
2024-10-09 09:40:13 -0700

    replaced "hadouken" sfx with "mana BOLT!"

--- js/Hadouken.js
+++ js/Hadouken.js
@@ -4,7 +4,8 @@ var hadoukenSprite = new Image();
 hadoukenSprite.src = "images/player_hadouken.png";
 hadoukenSprite.onload = function() { this.loaded = true; }
 
-const HADOUKEN_DAMAGE = 50;
+const HADOUKEN_DAMAGE = 45; // slightly less than half damage
+const HADOUKEN_SPEED = 0.75; // moves slow because it is very powerful and you only see it for one second
 
 var hadouken = {
     x:-9999,
@@ -15,7 +16,7 @@ var hadouken = {
         if (player.hadoukenSound) player.hadoukenSound.play();
         this.x = x;
         this.y = y;
-        this.speed = 1.5;
+        this.speed = HADOUKEN_SPEED;
     },
     update:function(dt) {
         this.x += this.speed * dt;
